10b5e37569e513a26a5cd475808241421f8d3e36
jeremij94 <jeremij94@gmail.com>
2022-11-01 14:27:53 -0500

    new changes made

---
+++ Testing/ProductRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepo : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepo(IDbConnection connection)
+        {
+            _conn = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
+
